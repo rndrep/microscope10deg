5d18c473182d903d96dde001a5567d1df7087fba
Your Name <hoshizone@gmail.com>
2021-10-22 14:25:33 +0700

    edited rotation mechanism changed

--- src/app/App.js
+++ src/app/App.js
@@ -11,7 +11,7 @@ import Wheel from "../components/Wheel";
 import Overlay from "../components/Overlay";
 
 // Assets
-import bcp from "../assets/img/xpl";
+import xpl from "../assets/img/xpl";
 import ppl from "../assets/img/ppl";
 
 import "./App.css";
@@ -20,14 +20,23 @@ gsap.registerPlugin(Draggable);
 
 const App = () => {
     const [rotation, setRotation] = useState(0);
-
+    const smooth = true;
+    const step = xpl.length === 36 ? 10 : 1;
     useEffect(() => {
         Draggable.create(".wheel", {
             type: "rotation",
-
+            minimumMovement: 1,
+            
             onDrag: function () {
+                console.log(this.rotation);
+                // градус поворота
                 setRotation(
-                    Math.floor(this.rotation / 10) * 10
+                    Math.floor(
+                        (this.rotation -
+                            360 * Math.floor(this.rotation / 360)) /
+                            10
+                    ) * 10
+
                     // this.rotation - 360 * Math.floor(this.rotation / 360)
                 );
             },
@@ -41,7 +50,11 @@ const App = () => {
                     <div className="microscope-container">
                         <Overlay name="plane polarised light" />
                         <Wheel />
-                        <Microscope images={ppl} rotation={rotation} />
+                        <Microscope
+                            images={ppl}
+                            rotation={rotation}
+                            smoothness={smooth}
+                        />
                     </div>
 
                     <p className="degrees">{Math.floor(rotation / 10) * 10}°</p>
@@ -49,7 +62,11 @@ const App = () => {
                     <div className="microscope-container">
                         <Overlay name="between crossed polars" />
                         <Wheel />
-                        <Microscope images={bcp} rotation={rotation} />
+                        <Microscope
+                            images={xpl}
+                            rotation={rotation}
+                            smoothness={smooth}
+                        />
                     </div>
                 </div>
             </div>
